81999ff92a8a406b4eef330a03672b3587d10954
laura <lpg.ies@gmail.com>
2019-04-04 09:09:15 +0200

    version 1.1 clase Dado

--- Dado.java
+++ Dado.java
@@ -1,7 +1,17 @@
+
 public class Dado {
 	private int valor;
 	
-	public void tirarDado(){
+	public Dado(){
+		;
+	}
+	public int tirarDado(){
 		//Este método dará un valor aleatorio al dado entre 1 y 6.
+		this.valor=(int)((Math.random()*6)+1);
+		return this.valor;
+	}
+	
+	public int getValor(){
+		return this.valor;
 	}
 }
